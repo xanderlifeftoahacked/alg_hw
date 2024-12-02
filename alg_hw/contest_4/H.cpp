336c65d44b21ddad6f7b162b72f565a7be1ced08
xanderlifeftoahacked <freztshow@gmail.com>
2024-12-02 23:12:44 +0300

    prober

--- alg_hw/contest_4/H.cpp
+++ alg_hw/contest_4/H.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 struct Pair {
@@ -11,27 +11,28 @@ struct Pair {
 int main() {
   size_t n = 0;
   std::cin >> n;
-
   std::vector<Pair> rates(n);
 
   for (size_t i = 0; i < n; ++i) {
     std::cin >> rates[i].a >> rates[i].b;
   }
 
-  std::vector<double> roubles(n);
-  std::vector<double> dollars(n);
-  std::vector<double> euro(n);
+  std::vector<double> roubles(n + 1);
+  std::vector<double> dollars(n + 1);
+  std::vector<double> euro(n + 1);
 
-  roubles[0] = 100.f;
-
+  roubles[0] = 100.0f;
   for (size_t i = 1; i < n + 1; ++i) {
     double r_usd = rates[i - 1].a;
     double r_eur = rates[i - 1].b;
 
-    roubles[i] = std::max({roubles[i - 1], dollars[i - 1] * r_usd, euro[i - 1] * r_eur});
-    dollars[i] = std::max({dollars[i - 1], roubles[i - 1] / r_usd, euro[i - 2] * (r_eur / r_usd)});
-    euro[i] = std::max({euro[i - 1], dollars[i - 1] * (r_usd / r_eur), roubles[i - 1] / r_eur});
+    roubles[i] =
+        std::max({roubles[i - 1], dollars[i - 1] * r_usd, euro[i - 1] * r_eur});
+    dollars[i] = std::max({dollars[i - 1], roubles[i - 1] / r_usd,
+                           euro[i - 1] * (r_eur / r_usd)});
+    euro[i] = std::max({euro[i - 1], dollars[i - 1] * (r_usd / r_eur),
+                        roubles[i - 1] / r_eur});
   }
 
-  std::cout << std::fixed << std::setprecision(3) << roubles[n] << '\n';
+  std::cout << std::fixed << std::setprecision(2) << roubles[n] << '\n';
 }
